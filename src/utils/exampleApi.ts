ef9bab053d22b8c5561349dbcbc6bc9c4cb6a2d0
dongchurl <churl@zzieut.com>
2023-04-02 14:31:39 +0900

    feat: 역할에 맞게 나눌 수 있는 폴더 구조를 생성합니다

---
+++ src/utils/exampleApi.ts
@@ -0,0 +1,15 @@
+export const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then(async (res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((result) => ({
+      name: result.name,
+      image: result.sprites["front_default"],
+      type: result.types.map((type: any) => type.type.name).join(", "),
+      id: result.id,
+    }));
+  });
+};
