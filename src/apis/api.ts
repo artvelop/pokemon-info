9267ca25becf5bed77a231db355b3ee084f897de
dongchurl <churl@zzieut.com>
2023-04-02 15:54:38 +0900

    feat: api 모듈을 추가합니다

---
+++ src/apis/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { localStorageKey } from '@/constants/localStorageKey';
+
+const API_TIMEOUT = 30000; // 30s
+const BASE_URL = 'https://pokeapi.co/api/v2';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  timeout: API_TIMEOUT,
+  withCredentials: false,
+});
+
+// authorization 토큰
+api.interceptors.request.use(
+  (config) => {
+    const jwtToken = localStorage.getItem(localStorageKey.JWT_TOKEN);
+    config.headers.Authorization = jwtToken ? `Bearer ${jwtToken}` : null;
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export default api;
---
+++ src/constants/localStorageKey.ts
@@ -0,0 +1,4 @@
+export const localStorageKey = {
+  JWT_TOKEN: 'JWT_TOKEN',
+  JWT_REFRESH_TOKEN: 'JWT_REFRESH_TOKEN',
+} as const;
